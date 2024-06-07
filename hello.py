95a18cbfc18eca514b130932bf363b6ee269ccaa
Your Name <you@example.com>
2024-06-07 10:59:52 +0200

    Updated code, templates and documentation to match versions and examples

---
+++ hello.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
